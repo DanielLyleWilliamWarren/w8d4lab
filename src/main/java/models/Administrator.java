7d075fc08333009c610b3cf97dcc56752264a583
jesus2102 <jesusperezmarin@gmail.com>
2018-07-12 13:35:27 +0100

    setup administrater done

--- src/main/java/models/Administrator.java
+++ src/main/java/models/Administrator.java
@@ -1,4 +1,40 @@
 package models;
 
-public class Administrator {
+import javax.persistence.*;
+
+@Entity
+@Table(name = "administrators")
+public class Administrator extends Employee{
+
+    private int id;
+    private Manager manager;
+
+    public Administrator(){}
+
+    public Administrator(String name, String nI, double salary, Manager manager){
+        super(name, nI, salary);
+        this.id = id;
+        this.manager = manager;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id", nullable = false)
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
 }
